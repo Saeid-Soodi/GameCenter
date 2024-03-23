b5a0b8f161d1ed777138f4784f08892e7e8f9bbf
Saeid Soodi <saeid.Soodi1998@gmail.com>
2024-03-24 00:07:53 +0330

    index added to contents

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.html'],
+  content: ['./public/**/*.html','./index.html'],
   theme: {
     extend: {},
   },
